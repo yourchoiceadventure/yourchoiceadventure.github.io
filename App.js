001881e1303d194988a309ef205bce2e9fa0b465
yourchoiceadventure <78973026+yourchoiceadventure@users.noreply.github.com>
2021-02-28 19:38:20 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+ import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='Mason'
+			userSecret='MAP12345'
+			projectID='a2dc65b1-457f-4060-bc6b-15fe714ff4a9'
+		/>
+	);
+}
